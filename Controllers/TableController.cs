0a43dee787e33b13f24983a57cf637917413af14
KolyaShabalinskiy <kolya.shabalinskiy@gmail.com>
2024-03-07 04:43:36 +0300

    initialization commit

---
+++ Controllers/TableController.cs
@@ -0,0 +1,25 @@
+﻿using MagicTable.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicTable.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableController : ControllerBase
+    {
+        public TableController() { }
+
+
+        [Route("{data}")]
+        public string Get(string data)
+        {
+            var content = new TableService().Get(data);
+            
+            return content;
+        }
+
+        
+    }
+}
---
+++ Services/TableService.cs
@@ -0,0 +1,21 @@
+﻿using MagicTable.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicTable.Services
+{
+    public class TableService 
+    {
+        public TableService() { }
+
+        public string Get(string dataKind)
+        {
+            string json = File.ReadAllText($"AppData/{dataKind}.json");          
+            
+            return json;
+        }
+
+    }
+
+    
+}
